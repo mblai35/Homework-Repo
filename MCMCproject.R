5e6a1c7bbbaa9f31bc7d6e238a34db479ab861e6
mblai35 <mbstrong35@gmail.com>
2016-12-04 15:09:32 -0700

    in progress

--- MCMCproject.R
+++ MCMCproject.R
@@ -1,8 +1,8 @@
 library(markovchain)
-library(ggplot)
+library(ggplot2)
 
 # Create transition matrix
-m <- matrix(c(.5, .5, 0, 0, 0,
+tMatrix <- matrix(c(.5, .5, 0, 0, 0,
               .25, .25, .5, 0, 0,
               0, 1/3, 1/6, .5, 0, 
               0, 0, .375, .125, .5,
@@ -10,7 +10,7 @@ m <- matrix(c(.5, .5, 0, 0, 0,
             ncol = 5, byrow = T)
 
 # Create a discrete time Markov Chain using the package "markovchain".
-MC1 <- as(m, "markovchain")
+MC1 <- as(tMatrix, "markovchain")
 
 # Set seed to prevent graph from changing. 
 set.seed(2)
@@ -21,14 +21,14 @@ plot(MC1, vertex.size = 20)
 summary(MC1)
 
 # Find the equilibrium distribution.
-steadyStates(MC1)[1, ]*15
+steadyStates(MC1)[1, ]
 
 # Create matrix with states and steady state solutions for plotting.
 # Multiply steady state solution by 15 to restore original values. 
-smat <- data.frame(MC1@states, steadyStates(MC1)[1, ]*15)
-colnames(smat) <- c('state', 'ss')
+SSmatrix <- data.frame(MC1@states, steadyStates(MC1)[1, ]*15)
+colnames(SSmatrix) <- c('state', 'ss')
 # Bar chart of population for each state. 
-qplot(state, data = smat, geom = "bar", weight = ss, 
+qplot(state, data = SSmatrix, geom = "bar", weight = ss, 
       ylab = "Population", xlab = "State", fill = state)
 #ggplot(smat, aes(state, ss)) + geom_point(aes(size = ss))
 
@@ -63,11 +63,12 @@ for (i in 1:steps)
   
 }
 
-# Plot movement over time. 
-#plot(movesMatrix[, 1], movesMatrix[, 2], type = 'l', 
- #    ylim = c(1,5), ylab = "States", xlab = "Position")
+#### Plot movement over time. 
+# Convert matrix to data.frame for ggplot2.
 movesMatrix <- data.frame(movesMatrix)
-ggplot(movesMatrix, aes(TimeStep, State)) + geom_path()
+plot1 <- ggplot(movesMatrix, aes(TimeStep, State)) + geom_path()
+# Plot bar chart of states.
+ggplot(movesMatrix, aes(State)) + geom_bar(aes(fill = State))
 
 # Repeat for another chain:
 # Metropolis Algorithm:
@@ -77,17 +78,12 @@ steps <- 100
 # Initial position. 
 current <- 3
 # Initialize vector to store moves. 
-movesMatrix <- matrix(rep(0, steps*2), ncol = 2)
-# Name movesMatrix columns.
-colnames(movesMatrix) <- c('TimeStep', 'State')
+movesMatrix$State2 <- c(rep(0, steps))
 
 for (i in 1:steps)
 {
-  # Record current time step.
-  movesMatrix[i, 1] <- i
-  
   # Record current position.
-  movesMatrix[i, 2] <- current
+  movesMatrix[i, 3] <- current
   
   # Simulate proposal to left or right with coin flip.
   proposal <- current + sample(c(-1, 1), size = 1)
@@ -101,9 +97,13 @@ for (i in 1:steps)
   
 }
 
-# Plot movement over time. 
-#plot(movesMatrix[, 1], movesMatrix[, 2], type = 'l', 
-#    ylim = c(1,5), ylab = "States", xlab = "Position")
-movesMatrix <- data.frame(movesMatrix)
-ggplot(movesMatrix, aes(TimeStep, State)) + geom_*()
+# Plot bar chart of states.
+ggplot(movesMatrix, aes(State2)) + geom_bar(aes(fill = State2))
+
+# Plot movement over time for both chains.  
+plot2 <- plot1 + geom_path(data = movesMatrix, aes(TimeStep,State2, 
+                                                   colour = "State2"))
+
+
+
 
