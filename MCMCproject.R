11e3d89cf1a89dbbdaf8656a02bc92f6ddc5588e
mblai35 <mbstrong35@gmail.com>
2016-12-04 22:22:24 -0700

    MCMC project updates

--- MCMCproject.R
+++ MCMCproject.R
@@ -1,38 +1,13 @@
+# MCMCproject.R
+# R version 3.2.2 (2015-08-14)
+# November 26, 2016. Mallory B. Lai.
+# Code to accompany MCMC ppt presentation. 
+#-----------------------------------------------------------------------
 library(markovchain)
 library(ggplot2)
+#-----------------------------------------------------------------------
 
-# Create transition matrix
-tMatrix <- matrix(c(.5, .5, 0, 0, 0,
-              .25, .25, .5, 0, 0,
-              0, 1/3, 1/6, .5, 0, 
-              0, 0, .375, .125, .5,
-              0, 0, 0, .4, .6), 
-            ncol = 5, byrow = T)
-
-# Create a discrete time Markov Chain using the package "markovchain".
-MC1 <- as(tMatrix, "markovchain")
-
-# Set seed to prevent graph from changing. 
-set.seed(2)
-# Graph MC1. 
-plot(MC1, vertex.size = 20)
-
-# Examine summary of MC1
-summary(MC1)
-
-# Find the equilibrium distribution.
-steadyStates(MC1)[1, ]
-
-# Create matrix with states and steady state solutions for plotting.
-# Multiply steady state solution by 15 to restore original values. 
-SSmatrix <- data.frame(MC1@states, steadyStates(MC1)[1, ]*15)
-colnames(SSmatrix) <- c('state', 'ss')
-# Bar chart of population for each state. 
-qplot(state, data = SSmatrix, geom = "bar", weight = ss, 
-      ylab = "Population", xlab = "State", fill = state)
-#ggplot(smat, aes(state, ss)) + geom_point(aes(size = ss))
-
-# Metropolis Algorithm:
+###### Metropolis Algorithm applied to States example:
 set.seed(3)
 # Number of steps for algorithm to make.
 steps <- 100
@@ -43,6 +18,7 @@ movesMatrix <- matrix(rep(0, steps*2), ncol = 2)
 # Name movesMatrix columns.
 colnames(movesMatrix) <- c('TimeStep', 'State')
 
+# Metropolis algorithm:
 for (i in 1:steps)
 {
   # Record current time step.
@@ -66,11 +42,31 @@ for (i in 1:steps)
 #### Plot movement over time. 
 # Convert matrix to data.frame for ggplot2.
 movesMatrix <- data.frame(movesMatrix)
-plot1 <- ggplot(movesMatrix, aes(TimeStep, State)) + geom_path()
+# Plot TimeStep as y-axis and State as x-axis and store as plot1.
+(plot1 <- ggplot(movesMatrix, aes(State, TimeStep)) + geom_path())
 # Plot bar chart of states.
-ggplot(movesMatrix, aes(State)) + geom_bar(aes(fill = State))
+(qplot1 <- qplot(State, data = movesMatrix, geom = "bar",
+      xlab = "State", fill = factor(State)))
 
-# Repeat for another chain:
+### Write plots to file for presentation: 
+# Set dimensions for plot1 TIFF file.
+tiff("MovesStatesEx1.tiff", width = 4, height = 8, 
+     units = 'in', res = 300)
+# Write plot to TIFF file. 
+plot1
+# Reset plotting environment. 
+dev.off()
+
+# Set dimensions for qplot1 TIFF file.
+tiff("BarChartStatesEx1.tiff", width = 4, height = 3, 
+     units = 'in', res = 300)
+# Write plot to TIFF file. 
+qplot1
+# Reset plotting environment. 
+dev.off()
+
+
+### Repeat for another chain:
 # Metropolis Algorithm:
 set.seed(4)
 # Number of steps for algorithm to make.
@@ -78,8 +74,9 @@ steps <- 100
 # Initial position. 
 current <- 3
 # Initialize vector to store moves. 
-movesMatrix$State2 <- c(rep(0, steps))
+movesMatrix$Chain2 <- c(rep(0, steps))
 
+# Metropolis algorithm:
 for (i in 1:steps)
 {
   # Record current position.
@@ -98,12 +95,60 @@ for (i in 1:steps)
 }
 
 # Plot bar chart of states.
-ggplot(movesMatrix, aes(State2)) + geom_bar(aes(fill = State2))
+(qplot1 <- qplot(Chain2, data = movesMatrix, geom = "bar",
+                 xlab = "State", fill = factor(Chain2)))
+
 
 # Plot movement over time for both chains.  
-plot2 <- plot1 + geom_path(data = movesMatrix, aes(TimeStep,State2, 
-                                                   colour = "State2"))
+(plot2 <- plot1 + geom_path(data = movesMatrix, 
+                           aes(Chain2, TimeStep, colour = "Chain2")))
+
+### Write plots to file for presentation: 
+# Set dimensions for plot2 TIFF file.
+tiff("MovesStatesEx2.tiff", width = 4, height = 8, 
+     units = 'in', res = 300)
+# Write plot to TIFF file. 
+plot2
+# Reset plotting environment. 
+dev.off()
+
+# Set dimensions for qplot1 TIFF file.
+tiff("BarChartStatesEx1.tiff", width = 4, height = 3, 
+     units = 'in', res = 300)
+# Write plot to TIFF file. 
+qplot1
+# Reset plotting environment. 
+dev.off()
 
 
+# Create transition matrix
+tMatrix <- matrix(c(.5, .5, 0, 0, 0,
+                    .25, .25, .5, 0, 0,
+                    0, 1/3, 1/6, .5, 0, 
+                    0, 0, .375, .125, .5,
+                    0, 0, 0, .4, .6), 
+                  ncol = 5, byrow = T)
 
+# Create a discrete time Markov Chain using the package "markovchain".
+MC1 <- as(tMatrix, "markovchain")
+
+# Set seed to prevent graph from changing. 
+set.seed(2)
+# Graph MC1. 
+plot(MC1, vertex.size = 20)
+
+# Examine summary of MC1
+summary(MC1)
+
+# Find the equilibrium distribution.
+steadyStates(MC1)[1, ]
+
+# Create matrix with states and steady state solutions for plotting.
+# Multiply steady state solution by 15 to restore original values. 
+SSmatrix <- data.frame(MC1@states, steadyStates(MC1)[1, ]*15)
+colnames(SSmatrix) <- c('state', 'ss')
+# Bar chart of population for each state. 
+qplot(state, data = SSmatrix, geom = "bar", weight = ss, 
+      ylab = "Population", xlab = "State", fill = state)
+#ggplot(smat, aes(state, ss)) + geom_point(aes(size = ss))
 
